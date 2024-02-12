149c9c4dfbae5f584afcf4f8cd36bb22e067a2dc
RuAstro <158161142+RuAstro@users.noreply.github.com>
2024-02-12 09:27:37 +0200

    added memory to store

--- Program.cs
+++ Program.cs
@@ -5,38 +5,46 @@ internal class Program
 {
 
     static void EnterValues(int numberOfValues, out double[] values, out char[] operators)
-    
-{
-    
-    values = new double[numberOfValues];
-    operators = new char[numberOfValues - 1];
+    {
+        values = new double[numberOfValues];
+        operators = new char[numberOfValues - 1];
 
+        Console.WriteLine($"Enter 1st value:");
+        values[0] = Convert.ToDouble(Console.ReadLine());
 
-    Console.WriteLine($"Enter 1st value:");
-    values[0] = Convert.ToDouble(Console.ReadLine());
-
-
-    for (int i = 1; i < numberOfValues; i++)
+        for (int i = 1; i < numberOfValues; i++)
         {
             Console.WriteLine($"Enter operator (+, -, *, /): ");
             operators[i - 1] = Convert.ToChar(Console.ReadLine());
 
             Console.WriteLine($"Enter {i + 1} value:");
             values[i] = Convert.ToDouble(Console.ReadLine());
+
         }
     }
 
+    //  static void StoreInMemory(double value)
+    // {
+    //     Memory = value;
+    //     Console.WriteLine($"Value {value} stored in memory.");
+    // }
 
-    private static void Main(string[] args)
+
+    public static void Main(string[] args)
     {
-        double[] values;
-        char[] operators;
-        double result = 0;
-        bool runProgram = false;
-       
+
+        bool runProgram = true;
 
         Console.WriteLine("Calculator Program");
         Console.WriteLine("--------------------");
+
+       do
+        {
+
+        double[] values;
+        char[] operators;
+        double result = 0;
+      
 
         Console.WriteLine("How many value numbers do you want to use?" + " Note: Only 2 - 100 values.");
         int numberOfValues = Convert.ToInt32(Console.ReadLine());
@@ -72,7 +80,7 @@ internal class Program
                     }
                     else
                     {
-                        Console.WriteLine("Error: Division by zero.");
+                        Console.WriteLine("Error: Division by zero will not execude.");
                     }
                     break;
                 default:
@@ -80,22 +88,22 @@ internal class Program
                     return;
             }
         }
-
         Console.WriteLine($"Your result: = {result}");
 
           Console.WriteLine("Would you like to do another calculation? (Yes, No): ");
 
-         if (Console.ReadLine() == "Yes")
+         if (Console.ReadLine().Equals("Yes", StringComparison.OrdinalIgnoreCase))
          {
             runProgram = true;
          }
-    
-           // (Console.ReadLine().Equals("Yes", StringComparison.OrdinalIgnoreCase));
-
          else
-             Console.WriteLine("Okay Bye!");
+         {
+           runProgram = false; 
+           Console.WriteLine("Okay Bye!"); 
+         }
+            
 
-
+        } while (runProgram);
              
 
         Console.ReadKey(); 
